c5471c4aa94bd151402b93c29fdb380b7f97a832
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-03-05 18:02:47 +0530

    learning

--- Subroutine.pl
+++ Subroutine.pl
@@ -1,8 +1,19 @@
 use Modern::Perl; 
 use feature "signatures";
 
+# New Way of defining a Subroutine
+
 sub add ($a, $b) {
     return $a + $b;
 }
 
-print add(1, 3),"\n";
+print add(1, 3), "\n";
+
+# Old fasined way of defining a Subroutine
+
+sub subtract {
+    my ($a, $b) = @_;
+    return $a - $b;
+}
+
+print subtract(4,2), "\n";
---
+++ std.pl
@@ -0,0 +1,12 @@
+use Modern::Perl;
+
+# STDIN, STDOUT, STDERR
+
+# STDIN is use to take a input from the keyboard
+
+my $input = <STDIN>;
+print $input;
+
+# OR it can read input from file as well
+my $input2 = <>;
+print $input2;
